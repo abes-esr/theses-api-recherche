584b60f942265b0116ee99e29c1b12137772c21c
gibert <gibert@abes.fr>
2022-09-15 12:32:37 +0200

    ajout de la gestion du CORS

--- src/main/java/fr/abes/thesesapirecherche/ThesesApiRechercheApplication.java
+++ src/main/java/fr/abes/thesesapirecherche/ThesesApiRechercheApplication.java
@@ -2,12 +2,37 @@ package fr.abes.thesesapirecherche;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class ThesesApiRechercheApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThesesApiRechercheApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean simpleCorsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		// *** URL below needs to match the Vue client URL and port ***
+
+		// Config for allowing CORS for all (local, dev, test, prod, external ...)
+		config.setAllowedOriginPatterns(Collections.singletonList("*"));
+		config.setAllowedMethods(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 
 }
