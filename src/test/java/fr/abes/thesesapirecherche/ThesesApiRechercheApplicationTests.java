fce97fe729cc59a236a254bf8261d41b688fef5c
Fabien Rétif <fabien.retif@zoho.com>
2022-10-18 15:41:16 +0200

    Ajout des tests sur les controllers

--- src/test/java/fr/abes/thesesapirecherche/ThesesApiRechercheApplicationTests.java
+++ src/test/java/fr/abes/thesesapirecherche/ThesesApiRechercheApplicationTests.java
@@ -1,12 +1,18 @@
 package fr.abes.thesesapirecherche;
 
 import fr.abes.thesesapirecherche.security.SpringSecurityConfig;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 @SpringBootTest(classes = {ThesesApiRechercheApplication.class, HandlerMappingIntrospector.class, SpringSecurityConfig.class})
@@ -19,4 +25,12 @@ public class ThesesApiRechercheApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    @DisplayName("Route inexistante")
+    public void doRouteInexistante() throws Exception {
+        mockMvc.perform(post("/api"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.status").value("NOT_FOUND"))
+                .andExpect(jsonPath("$.message").exists());
+    }
 }
