c5ed024c36046b866c994f5d20fdf66e58b282b4
gibert <gibert@abes.fr>
2022-11-03 17:35:49 +0100

    - suppression du service titreEtResume - changement de l'url "recherche" par défaut - passage en queryString de recherche sur titre

--- src/main/java/fr/abes/thesesapirecherche/builder/SearchQueryBuilder.java
+++ src/main/java/fr/abes/thesesapirecherche/builder/SearchQueryBuilder.java
@@ -1,6 +1,7 @@
 package fr.abes.thesesapirecherche.builder;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.query_dsl.Operator;
 import co.elastic.clients.elasticsearch._types.query_dsl.Query;
 import co.elastic.clients.elasticsearch._types.query_dsl.QueryStringQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.TermQuery;
@@ -82,48 +83,12 @@ public class SearchQueryBuilder {
 
     public String rechercheSurLeTitre (String chaine, Integer page, Integer nombre) throws Exception {
 
-        SearchResponse<These> response = this.getElasticsearchClient().search(s -> s
-                        .index("theses-sample")
-                        .query(q->q
-                                .match(t->t
-                                        .query(chaine)
-                                        .field("titrePrincipal")))
-                        .from(page)
-                        .size(nombre),
-                These.class
-        );
-
-        /* la réponse est récupérable :
-
-        1) via un tableau d'objets java :
-
-        List<Hit<These>> theses = response.hits().hits();
-        for (Hit<These> hit : theses) {
-            These these = hit.source();
-            reponse += these.getTitre(); etc.
-        }
-
-        2) directement en json :
-         */
-        final StringWriter writer = new StringWriter();
-        try (final JacksonJsonpGenerator generator = new JacksonJsonpGenerator(new JsonFactory().createGenerator(writer))) {
-            response.serialize(generator, new JacksonJsonpMapper());
-        }
-        return writer.toString();
-    }
-
-    public String rechercheSurLeTitreEtResume (String chaine, String termeRameau) throws Exception {
-
         QueryStringQuery.Builder builderQuery = new QueryStringQuery.Builder();
         builderQuery.query(chaine);
-        builderQuery.fields("titre^5","abstractFR");
+        builderQuery.defaultOperator(Operator.And);
+        builderQuery.fields("titrePrincipal");
         builderQuery.quoteFieldSuffix(".exact");
         Query query = builderQuery.build()._toQuery();
-
-        TermQuery.Builder builderTerm = new TermQuery.Builder();
-        builderTerm.field("rameau");
-        builderTerm.value(termeRameau);
-        TermQuery termQuery = builderTerm.build();
 
         SearchResponse<These> response = this.getElasticsearchClient().search(
                 s -> s
@@ -131,11 +96,12 @@ public class SearchQueryBuilder {
                         .query(q->q
                                 .bool(t-> t
                                         .must(query)
-                                        .filter(a->a
-                                                .term(termQuery))
-                                )),
+                                ))
+                        .from(page)
+                        .size(nombre),
                 These.class
         );
+
         final StringWriter writer = new StringWriter();
         try (final JacksonJsonpGenerator generator = new JacksonJsonpGenerator(new JsonFactory().createGenerator(writer))) {
             response.serialize(generator, new JacksonJsonpMapper());
--- src/main/java/fr/abes/thesesapirecherche/controller/SearchThesesController.java
+++ src/main/java/fr/abes/thesesapirecherche/controller/SearchThesesController.java
@@ -9,13 +9,13 @@ import java.util.Optional;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/recherche/")
 public class SearchThesesController {
 
     @Autowired
     SearchQueryBuilder searchQueryBuilder;
 
-    @GetMapping(value = "/recherche/")
+    @GetMapping(value = "/titre/")
     public String rechercheSurLeTitre(@RequestParam final String q,
                                       @RequestParam Optional<Integer> page,
                                       @RequestParam Optional<Integer> nombre) throws Exception {
@@ -25,18 +25,6 @@ public class SearchThesesController {
                 return searchQueryBuilder.rechercheSurLeTitre(q, page.get(), nombre.get());
             else
                 return searchQueryBuilder.rechercheSurLeTitre(q, 0, 10);
-
-        } catch (Exception e) {
-            log.error(e.toString());
-            throw e;
-        }
-    }
-
-    @GetMapping(value = "/recherche/titreEtResume/")
-    public String rechercheSurLeTitreEtResume(@RequestParam final String q, @RequestParam final String rameau) throws Exception {
-        log.info("debut de rechercheSurLeTitreEtResume...");
-        try {
-            return searchQueryBuilder.rechercheSurLeTitreEtResume(q, rameau);
 
         } catch (Exception e) {
             log.error(e.toString());
