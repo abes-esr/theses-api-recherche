dd4c822838ee5813f8aaf0545ecc79474b7d5a63
Théo Chambon <chambon@abes.fr>
2023-05-30 12:07:38 +0200

    Ajout recherche ppn rameau

--- src/main/java/fr/abes/thesesapirecherche/theses/builder/SearchQueryBuilder.java
+++ src/main/java/fr/abes/thesesapirecherche/theses/builder/SearchQueryBuilder.java
@@ -107,7 +107,7 @@ public class SearchQueryBuilder {
         QueryStringQuery.Builder builderQuery = new QueryStringQuery.Builder();
         builderQuery.query(chaine);
         builderQuery.defaultOperator(Operator.And);
-        builderQuery.fields("resumes.*^30", "titres.*^30", "nnt^15", "discipline^15", "^15", "sujets^15", "auteursNP^12", "directeursNP^2", "ecolesDoctoralesN^5", "etabSoutenanceN^5", "oaiSets^5", "etabsCotutelleN^1", "membresJuryNP^1", "partenairesRechercheN^1", "presidentJuryNP^1", "rapporteurs^1");
+        builderQuery.fields("resumes.*^30", "titres.*^30", "nnt^15", "discipline^15", "sujetsRameauPpn^15", "sujetsRameauLibelle^15", "sujets^15", "auteursNP^12", "directeursNP^2", "ecolesDoctoralesN^5", "etabSoutenanceN^5", "oaiSets^5", "etabsCotutelleN^1", "membresJuryNP^1", "partenairesRechercheN^1", "presidentJuryNP^1", "rapporteurs^1");
         builderQuery.quoteFieldSuffix(".exact");
 
         return builderQuery.build()._toQuery();
