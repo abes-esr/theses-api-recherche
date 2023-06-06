da62dc67cebe1f7996b19eb974873963a4f4ac2a
Fabien Rétif <fabien.retif@zoho.com>
2023-06-06 18:38:03 +0200

    Mise à jour des TUs pour la pondération v3 des personnes

--- src/test/java/fr/abes/thesesapirecherche/personnes/controller/recherche/RecherchePonderationTest.java
+++ src/test/java/fr/abes/thesesapirecherche/personnes/controller/recherche/RecherchePonderationTest.java
@@ -22,14 +22,14 @@ public class RecherchePonderationTest extends PersonneControllerTest {
     @DisplayName("Rechercher des personnes au nom de Rousseau avec la pondération sur l'identifiant IdRef, le role, le nombre de thèses et les thèses récentes")
     @EnableOnIntegrationTest
     public void rechercherRousseauBoostIdrefRoleNbThesesThesesRecentes() throws Exception {
-        mockMvc.perform(get("/api/v1/tests/personnes/recherche/?q=rousseau&index=per_recherche_simple_rousseau_ponderationv2"))
+        mockMvc.perform(get("/api/v1/tests/personnes/recherche/?q=rousseau&index=per_affichage_theses_roles_etabs_disc_ponderationv3"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.totalHits").value(6))
-                .andExpect(jsonPath("$.personnes[0].id").value("050646176"))
-                .andExpect(jsonPath("$.personnes[1].id").value("081954522"))
+                .andExpect(jsonPath("$.personnes[0].id").value("081954522"))
+                .andExpect(jsonPath("$.personnes[1].id").value("050646176)"))
                 .andExpect(jsonPath("$.personnes[2].id").value("098248782"))
                 .andExpect(jsonPath("$.personnes[3].id").value("128704802"))
-                .andExpect(jsonPath("$.personnes[4].id").value("127566635"))
+                .andExpect(jsonPath("$.personnes[4].id").value("081810652"))
                 .andExpect(jsonPath("$.personnes[5].id").value("081810652"))
                 .andExpect(jsonPath("$.personnes[6].has_idref").value(false))
                 .andExpect(jsonPath("$.personnes[6].nom").value("Rousseau"))
@@ -43,7 +43,7 @@ public class RecherchePonderationTest extends PersonneControllerTest {
     @DisplayName("Rechercher des personnes au prénom de Erwan avec la pondération sur l'identifiant IdRef et le role")
     @EnableOnIntegrationTest
     public void rechercherErwanBoostIdrefRoleNbThesesThesesRecentes() throws Exception {
-        mockMvc.perform(get("/api/v1/tests/personnes/recherche/?q=erwan&index=per_recherche_simple_rousseau_ponderationv2"))
+        mockMvc.perform(get("/api/v1/tests/personnes/recherche/?q=erwan&index=per_affichage_theses_roles_etabs_disc_ponderationv3"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.totalHits").value(4))
                 .andExpect(jsonPath("$.personnes[0].id").value("098248782"))
