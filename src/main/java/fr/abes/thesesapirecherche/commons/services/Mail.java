5b22bb74810a982758aca90916821013e290f801
theo-chmbn <chambon@abes.fr>
2023-07-26 10:19:27 +0200

    Ajout signature mail

--- src/main/java/fr/abes/thesesapirecherche/commons/services/Mail.java
+++ src/main/java/fr/abes/thesesapirecherche/commons/services/Mail.java
@@ -16,13 +16,28 @@ public class Mail {
 
     public static String sendMail(String wsMailURL, List to, String mailTheses, String domaine, String url, String nom, String prenom, String mail, String objet, String question, String appSource) throws JsonProcessingException {
         RestTemplate restTemplate = new RestTemplate();
+        MailJSON mailJSON;
 
-        String subject = "[thesesFr] -- " + objet + " -- Message de " + nom + " " + prenom;
-        MailJSON mailJSON = new MailJSON(to, new ArrayList<>() {{
-            add(mail);
-        }}, new ArrayList<>() {{
-            add(mailTheses);
-        }}, subject, MailTemplates.mailSignalerErreur(domaine, url, nom, prenom, mail, objet, question, appSource));
+        // Si on a trouvé un destinataire en BDD
+        if (!to.isEmpty()) {
+            String subject = "[thesesFr] -- " + objet + " -- Message de " + nom + " " + prenom;
+            mailJSON = new MailJSON(to, new ArrayList<>() {{
+                add(mail);
+            }}, new ArrayList<>() {{
+                add(mailTheses);
+            }}, subject, MailTemplates.mailSignalerErreur(domaine, url, nom, prenom, mail, objet, question, appSource));
+        }
+        // Sinon on envoi un mail d'erreur à l'adresse theses
+        else {
+            String subject = "[thesesFr] -- Erreur d’adressage de l’assistance déportée theses.fr";
+            mailJSON = new MailJSON(new ArrayList<>() {{
+                add(mailTheses);
+            }}, new ArrayList<>() {
+            }, new ArrayList<>() {
+            }, subject, MailTemplates.mailSignalerErreurNoRecipient(domaine, url, nom, prenom, mail, objet, question, appSource));
+        }
+
+
         ObjectMapper mapper = new ObjectMapper();
         String json = mapper.writeValueAsString(mailJSON);
 
@@ -35,7 +50,6 @@ public class Mail {
         return restTemplate.postForObject(wsMailURL, entity, String.class);
     }
 
-    ;
 
     public record MailJSON(List to, List cc, List cci, String subject, String text) {
     }
