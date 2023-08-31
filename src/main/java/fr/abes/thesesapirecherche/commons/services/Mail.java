263d3f329fcd0d55ac3f2bff430d4bfce7e19028
theo-chmbn <chambon@abes.fr>
2023-08-31 11:25:58 +0200

    Envoi mail en double en cas d'erreur

--- src/main/java/fr/abes/thesesapirecherche/commons/services/Mail.java
+++ src/main/java/fr/abes/thesesapirecherche/commons/services/Mail.java
@@ -17,6 +17,9 @@ public class Mail {
     public static String sendMail(String wsMailURL, List to, String mailTheses, String domaine, String url, String nom, String prenom, String mail, String objet, String question, String appSource) throws JsonProcessingException {
         RestTemplate restTemplate = new RestTemplate();
         MailJSON mailJSON;
+        ObjectMapper mapper = new ObjectMapper();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
         // Si on a trouvé un destinataire en BDD
         if (!to.isEmpty()) {
@@ -38,6 +41,14 @@ public class Mail {
             }}, new ArrayList<>() {
             }, subject, MailTemplates.mailSignalerErreur(domaine, url, nom, prenom, mail, objet, question, appSource));
 
+            mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(mailJSON);
+
+            restTemplate.getMessageConverters()
+                    .add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+            HttpEntity<String> entity = new HttpEntity<String>(json, headers);
+            restTemplate.postForObject(wsMailURL, entity, String.class);
+
             //mail d'erreur
             String subjectErr = "[thesesFr] -- Erreur d’adressage de l’assistance déportée theses.fr";
             mailJSON = new MailJSON(new ArrayList<>() {{
@@ -47,12 +58,8 @@ public class Mail {
             }, subjectErr, MailTemplates.mailSignalerErreurNoRecipient(domaine, url, nom, prenom, mail, objet, question, appSource));
         }
 
-
-        ObjectMapper mapper = new ObjectMapper();
+        mapper = new ObjectMapper();
         String json = mapper.writeValueAsString(mailJSON);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
 
         restTemplate.getMessageConverters()
                 .add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8));
