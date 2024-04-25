f1b581dd516d4ff7deb38b68a0c28dc3ef7338ea
theo-chmbn <chambon@abes.fr>
2024-04-25 14:23:38 +0200

    Ajout mode maintenance

---
+++ src/main/java/fr/abes/thesesapirecherche/filters/MaintenanceFilter.java
@@ -0,0 +1,33 @@
+package fr.abes.thesesapirecherche.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(1)
+public class MaintenanceFilter implements Filter {
+
+    @Value("${maintenance}")
+    private boolean isMaintenance;
+
+    @Value("${maintenance.message}")
+    private String maintenanceMsg;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        if (isMaintenance) {
+            httpResponse.setStatus(HttpServletResponse.SC_SERVICE_UNAVAILABLE);
+            httpResponse.getWriter().write(maintenanceMsg);
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+}
